c9a6a1fb8e655165361ced14e507bae4c8c30313
Yuto-79 <24144024@ed.tmu.ac.jp>
2026-07-08 19:48:53 +0900

    Initialize repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def hello
-    render html: "Hello, world!"
-  end
-
-  def goodbye
-    render html: "Goodbye, world!"
-  end
-
-  def different
-    render html: "hola, mundo!"
-  end
 end
